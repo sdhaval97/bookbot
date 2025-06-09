fc01dac8d636019a9725dab538bbaccf624606fb
sdhaval97 <sdhaval97@gmail.com>
2025-06-09 17:58:06 +0000

    added character count functionality

--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_num_words(text):
 	return len(text.split())
 
+def get_char_count(text: str) -> dict:
+    """Return a dictionary of character counts (case-insensitive)."""
+    char_count = {}
+    for char in text.lower():
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+
